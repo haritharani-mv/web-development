ded609f59a2146ba1573f250809404f3d2e45a9e
haritharani-mv <haritharani.mv@gmail.com>
2020-07-30 21:32:43 +0530

    modification in facebook page

---
+++ string.js
@@ -0,0 +1,10 @@
+var name1 = "Haritha";
+var name2 = new String("Haritha");
+if (name1 === name2)
+{
+	console.log("Result is true");
+}
+else
+{
+	console.log("Result is false");
+}
--- try-throw.js
+++ try-throw.js
@@ -1,13 +1,13 @@
 function myError()
 {
-var x,msg;
+var msg, x;
 msg = document.getElementById("msg");
 msg.innerHTML = "";
-x = getElementById("some").value;
+x = document.getElementById("some").value;
 try
 {
 if (x == "") throw "enter a number";
-if (x = isNaN(x)) throw "please enter a number";
+if (isNaN(x)) throw "please enter a number";
 if (x < 4) throw "enter a num greater than 3";
 }
 catch(err)

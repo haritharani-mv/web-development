145e1d4bf634536379f623d589f009f6c498fcb6
haritharani-mv <haritharani.mv@gmail.com>
2020-08-04 10:39:36 +0530

    modification in facebook page

--- string.js
+++ string.js
@@ -1,5 +1,14 @@
-var name1 = "Haritha";
-var name2 = new String("Haritha");
+var sampleString = 'lets start your concept';
+console.log(sampleString.indexOf('start'));
+console.log(sampleString.lastIndexOf('start'));
+console.log(sampleString.slice(5,11));
+console.log(sampleString.replace("hari","rani");
+console.log(sampleString.toLowerCase());
+console.log(sampleString.toUpperrCase());
+
+
+var name1 = "Kevin";
+var name2 = new String("Kevin");
 if (name1 === name2)
 {
 	console.log("Result is true");

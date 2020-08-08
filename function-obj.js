40097e69f786f1f681b5905a18ec351d2acb09aa
haritharani-mv <haritharani.mv@gmail.com>
2020-08-08 21:12:18 +0530

    modification in facebook page

---
+++ function-obj.js
@@ -0,0 +1,16 @@
+var harley = {
+		name : "haritha",
+		frd : "kal",
+		getBudgetForCustomStuff : function(a)
+		{
+		 return("you need to get lower budget",
+		 console.log( this.name )
+		)
+	      },
+		shoutOuts : [
+		{ thumsup : "fav drink" },
+		{ rose : "fav flower" }
+		]
+};
+harley.getBudgetForCustomStuff(8000);
+harley.shoutOuts;

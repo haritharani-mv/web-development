3b6db74e6aad1fe23e93fbacf41a21d1d9c0d08e
haritharani-mv <haritharani.mv@gmail.com>
2020-08-08 21:29:06 +0530

    modification in facebook page

--- function-obj.js
+++ function-obj.js
@@ -1,16 +1,4 @@
-var harley = {
-		name : "haritha",
-		frd : "kal",
-		getBudgetForCustomStuff : function(a)
-		{
-		 return("you need to get lower budget",
-		 console.log( this.name )
-		)
-	      },
-		shoutOuts : [
-		{ thumsup : "fav drink" },
-		{ rose : "fav flower" }
-		]
-};
-harley.getBudgetForCustomStuff(8000);
-harley.shoutOuts;
+var person = {name:"John", age:30, city:"New York"};
+
+document.getElementById("test").innerHTML = person;
+

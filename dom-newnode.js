435da1f5940a15e4563d9cfa488cca16c8be9807
haritharani-mv <haritharani.mv@gmail.com>
2020-08-15 11:32:20 +0530

    modification in facebook page

---
+++ dom-newnode.js
@@ -0,0 +1,6 @@
+var newList = document.createElement("li");
+newList.innerHTML = "new nodes are created";
+document.getElementById("change").appendChild(newList);
+document.getElementById("test").innerHTML = "dom";
+ 
+
